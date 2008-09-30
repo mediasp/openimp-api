8c25e4ffd9e40e6211a49ec66312823c88d49975
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2008-09-30 17:46:35 +0100

    beginnings of File::Image subclass. hlated for now as the API for this is very buggy

--- lib/ci/file/image.rb
+++ lib/ci/file/image.rb
@@ -1,2 +1,15 @@
 class CI::File::Image < CI::File
+  ci_properties :Width, :Height
+  
+  RESIZE_METHODS = [
+    'NOMODIFIER',
+    'EXACT',
+    'SQUARE',
+    'SMALLER',
+    'LARGER'
+  ]
+  
+  def resize(target_width, target_height, method='NOMODIFIER')
+    raise  "resize method not recognised" unless RESIZE_METHODS.include?(method)
+  end
 end
