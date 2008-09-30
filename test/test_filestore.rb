b735e3d7b275e813c36e80cb129f8030e07bf215
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2008-09-30 17:45:54 +0100

    remove redundant file token class, more filestore unit tests

--- test/test_filestore.rb
+++ test/test_filestore.rb
@@ -6,12 +6,12 @@ class TestCI < Test::Unit::TestCase
   def setup
     CI.username = 'example@ci-support.com'
     CI.password = 'example'
+    c = CI::File.new_from_file(TEST_ASSET_FILE, 'text/plain')
+    c.store
+    @uploaded_id = c.id
+    @uploaded_data = c.data
   end
   
-  def test_truth
-    assert true
-  end
-
   def test_upload_file
     c = CI::File.new_from_file(TEST_ASSET_FILE, 'text/plain')
     assert_instance_of CI::File, c
@@ -25,13 +25,15 @@ class TestCI < Test::Unit::TestCase
     c.data = nil
     c.retrieve
     assert_equal c.data, original_data
-    @uploaded_id = c.id
-    @uploaded_data = c.data
   end  
 
   def test_find_file
     c = CI::File.find(@uploaded_id)
     assert_instance_of CI::File, c
+    assert_not_nil c.sha1_digest_base64
+    assert_not_nil c.mime_minor
+    assert_not_nil c.mime_major
+    assert_equal "/filestore/#{@uploaded_id}", c.__representation__
     assert_equal c.id, @uploaded_id
     assert_equal c.data, @uploaded_data
   end
