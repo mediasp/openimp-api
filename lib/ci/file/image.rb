6a2e23480a64b5b912d9c9c17d0c67e888787ec1
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2008-10-02 14:59:48 +0100

    alter post params logic to not try and guess at CI properties given a ruby-ish hash key, add Image resizing functionality and test.

--- lib/ci/file/image.rb
+++ lib/ci/file/image.rb
@@ -9,7 +9,30 @@ class CI::File::Image < CI::File
     'LARGER'
   ]
   
-  def resize(target_width, target_height, method='NOMODIFIER')
+  RESIZE_TYPES = {
+    'jpeg' => 'jpg',
+    'png' => 'png',
+    'tiff' => 'tiff',
+    'gif' => 'gif'
+  }
+  
+  def initialize(params={}, data=nil, mime_type=nil)
+    super(params, data, mime_type)
+    cast_as(self.class) unless __class__ == 'API::File::Image' #Looks tautological, but we need to let the server know that this is an Image.
+  end
+  
+  def resize(target_width=nil, target_height=nil, method=nil, target_type=nil, synchronous=true)
+    self.store
+    method ||= 'NOMODIFIER'
+    target_width ||= width
+    target_height ||= height
+    target_type ||= RESIZE_TYPES[mime_minor]
+    target_type = target_type.to_s #if a symbol was passed.
+    raise "target image type not recognised" unless RESIZE_TYPES.values.include?(target_type)
     raise  "resize method not recognised" unless RESIZE_METHODS.include?(method)
+    method = "IMAGE_RESIZE_#{method}"
+    params = {:targetY => target_height, :targetX =>target_width, :targetType => target_type, :resizeType => method}
+    params.merge!(:Synchronous => 'on') if synchronous
+    CI::File::Image.do_request(:post, "/#{id}/contextualmethod/Resize", nil, nil, params)
   end
 end
--- test/test_filestore.rb
+++ test/test_filestore.rb
@@ -9,7 +9,6 @@ class TestFilestore < Test::Unit::TestCase
     CI.password = 'foo52bim'
     c = CI::File.new_from_file(TEST_ASSET_FILE, 'text/plain')
     c.store
-    @test_image = CI::File.new_from_file(TEST_IMAGE_FILE, 'image/jpeg')
     @uploaded_id = c.id
     @uploaded_data = c.data
   end
@@ -58,9 +57,22 @@ class TestFilestore < Test::Unit::TestCase
     assert_equal file.data, open(token.url) {|r| r.read}
   end
    
-  def test_images
-    image = @test_image.cast_as(CI::File::Image)
+  def test_cast_to_image
+    test_image = CI::File.new_from_file(TEST_IMAGE_FILE, 'image/jpeg')
+    image = test_image.cast_as(CI::File::Image)
     assert_instance_of CI::File::Image, image
+    assert_not_nil image.height
+    assert_not_nil image.width
   end
-   
+  
+  def test_image_resize
+    test_image = CI::File::Image.new_from_file(TEST_IMAGE_FILE, 'image/jpeg')
+    assert_instance_of CI::File::Image, test_image
+    height = 600
+    width = 800
+    resized = test_image.resize(width, height)
+    assert_instance_of CI::File::Image, resized
+    assert_equal height, resized.height
+    assert_equal width, resized.width
+  end
 end
